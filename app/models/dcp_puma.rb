72434741e05f0724334a2bc05b9e7266e73d6a41
Mac <mac@MacBook-Pro.local>
2018-10-30 13:36:26 -0500

    dcp_puma for age groups table displays correct data, needs styling

--- app/models/dcp_puma.rb
+++ app/models/dcp_puma.rb
@@ -1,4 +1,12 @@
 class DcpPuma < ApplicationRecord
   belongs_to :boro
   has_many :pums
+
+  scope :under_18, -> { where agegroups: '1' }
+  scope :yr18_24, -> { where agegroups: '2' }
+  scope :yr25_34, -> { where agegroups: '3' }
+  scope :yr35_44, -> { where agegroups: '4' }
+  scope :yr45_64, -> { where agegroups: '5' }
+  scope :over_65, -> { where agegroups: '6' }
+  
 end
--- app/models/pum.rb
+++ app/models/pum.rb
@@ -2,6 +2,19 @@ class Pum < ApplicationRecord
   belongs_to :dcp_puma
   belongs_to :boro
 
+  scope :under_18, -> { where agegroups: '1' }
+  scope :yr18_24, -> { where agegroups: '2' }
+  scope :yr25_34, -> { where agegroups: '3' }
+  scope :yr35_44, -> { where agegroups: '4' }
+  scope :yr45_64, -> { where agegroups: '5' }
+  scope :over_65, -> { where agegroups: '6' }
+
+  scope :mn, -> { where borough: '1' }
+  scope :bx, -> { where borough: '2' }
+  scope :bk, -> { where borough: '3' }
+  scope :qn, -> { where borough: '4' }
+  scope :si, -> { where borough: '5' }
+
   def self.import(file)
     # pums = []
     CSV.foreach(file.path, headers: true) do |row|
