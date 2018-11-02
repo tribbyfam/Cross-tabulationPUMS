a947565de7eeac5c2f5be2fe1f7a7da5368f91a0
Mac <mac@MacBook-Pro-2.fios-router.home>
2018-11-01 22:58:29 -0500

    tried to implement percentages for index boro page

--- app/controllers/boros_controller.rb
+++ app/controllers/boros_controller.rb
@@ -40,10 +40,27 @@ class BorosController < ApplicationController
     @boros = Boro.all
     @pums = Pum.all
     @total_wnh = (@pums.mn.wnh.count + @pums.bx.wnh.count + @pums.bk.wnh.count + @pums.qn.wnh.count + @pums.si.wnh.count) * 100 / @pums.count(:mutu).to_f
+
     @total_bnh = (@pums.mn.bnh.count + @pums.bx.bnh.count + @pums.bk.bnh.count + @pums.qn.bnh.count + @pums.si.bnh.count) * 100 / @pums.count(:mutu).to_f
+
     @total_his = (@pums.mn.his.count + @pums.bx.his.count + @pums.bk.his.count + @pums.qn.his.count + @pums.si.his.count) * 100 / @pums.count(:mutu).to_f
+
     @total_onh = (@pums.mn.onh.count + @pums.bx.onh.count + @pums.bk.onh.count + @pums.qn.onh.count + @pums.si.onh.count) * 100 / @pums.count(:mutu).to_f
+
     @total_anh = (@pums.mn.anh.count + @pums.bx.anh.count + @pums.bk.anh.count + @pums.qn.anh.count + @pums.si.anh.count) * 100 / @pums.count(:mutu).to_f
+
     @total_twonh = (@pums.mn.twonh.count + @pums.bx.twonh.count + @pums.bk.twonh.count + @pums.qn.twonh.count + @pums.si.twonh.count) * 100 / @pums.count(:mutu).to_f
+
+    @total_under_18 = (@pums.mn.under_18.count + @pums.bx.under_18.count + @pums.bk.under_18.count + @pums.qn.under_18.count + @pums.si.under_18.count)
+
+    @total_yr18_24 = (@pums.mn.yr18_24.count + @pums.bx.yr18_24.count + @pums.bk.yr18_24.count + @pums.qn.yr18_24.count + @pums.si.yr18_24.count) * 100 / @pums.count(:agegroups).to_f
+
+    @total_yr25_34 = (@pums.mn.yr25_34.count + @pums.bx.yr25_34.count + @pums.bk.yr25_34.count + @pums.qn.yr25_34.count + @pums.si.yr25_34.count) * 100 / @pums.count(:agegroups).to_f
+
+    @total_yr35_44 = (@pums.mn.yr35_44.count + @pums.bx.yr35_44.count + @pums.bk.yr35_44.count + @pums.qn.yr35_44.count + @pums.si.yr35_44.count) * 100 / @pums.count(:agegroups).to_f
+
+    @total_yr45_64 = (@pums.mn.yr45_64.count + @pums.bx.yr45_64.count + @pums.bk.yr45_64.count + @pums.qn.yr45_64.count + @pums.si.yr45_64.count) * 100 / @pums.count(:agegroups).to_f
+
+    @total_over_65 = (@pums.mn.over_65.count + @pums.bx.over_65.count + @pums.bk.over_65.count + @pums.qn.over_65.count + @pums.si.over_65.count) * 100 / @pums.count(:agegroups).to_f
   end
 end
